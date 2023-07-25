b920d10642955fd837e59b50c06c22bc087a2c03
unamiatoii <pierrejaphetkonan@gmail.com>
2023-07-25 22:28:38 +0000

    Modification loginPage

--- lib/Pages/Auth/LoginPage.dart
+++ lib/Pages/Auth/LoginPage.dart
@@ -1,5 +1,7 @@
 import 'package:finance_me/Pages/Auth/Register.dart';
 import 'package:flutter/material.dart';
+import 'package:passwordfield/passwordfield.dart';
+// Import de la bibliothèque d'icônes
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -14,75 +16,90 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Text(
-                'Hello,Againt!',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 22.0),
+              padding: EdgeInsets.all(0.0),
+              child: Icon(
+                Icons.person_outline, // Utilisation de l'icône person
+                size: 70.0,
+                color: Colors.purple,
               ),
             ),
-            const Text('Welcom back, you are been missed'),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Hello Again!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Text('Welcome back, you are been missed'),
             const SizedBox(
               height: 20.0,
             ),
+
+            //Input E-mail
             Padding(
-              padding: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(10.0),
               child: Container(
-                padding: EdgeInsets.all(8.0),
-                height: 50,
                 width: MediaQuery.of(context).size.width,
                 decoration: const BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(10.0)),
                     color: Colors.white),
                 child: const TextField(
                   decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Enter Name',
-                    prefixIcon: Icon(Icons.email_outlined)
+                    prefixIcon: Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                    contentPadding: EdgeInsets.zero,
+                    hintText: 'E-Mail',
                   ),
                 ),
               ),
             ),
+
             Padding(
-              padding: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(10.0),
               child: Container(
-                padding: const EdgeInsets.all(8.0),
-                height: 50,
                 width: MediaQuery.of(context).size.width,
                 decoration: const BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(10.0)),
                     color: Colors.white),
-                child:  const TextField(
-                  obscureText: true,
+                child: const TextField(
                   decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: '**************',
+                    suffixIcon: Icon(Icons.remove_red_eye_outlined),
                     prefixIcon: Icon(Icons.key),
-                    suffixIcon: Icon(Icons.remove_red_eye_outlined)
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                    contentPadding: EdgeInsets.zero,
+                    hintText: '********',
                   ),
                 ),
               ),
             ),
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                    Expanded(child: Container()),
-         
-              const Padding(
-                padding: EdgeInsets.only(right: 20.0),
-                child:  TextButton(onPressed:null,
-                child: Text('mot de passe oublier !',style: TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,fontSize: 14.0
-                ),)),
-              ),
+                Expanded(child: Container()),
+                const Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: TextButton(
+                      onPressed: null,
+                      child: Text(
+                        'Mot de passe oublié !',
+                        style: TextStyle(
+                            color: Colors.redAccent,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0),
+                      )),
+                ),
               ],
             ),
-
-          
             InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder:(context) => RegisterPage()));
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()));
               },
               child: Padding(
                 padding: const EdgeInsets.all(12.0),
@@ -91,13 +108,17 @@ class LoginPage extends StatelessWidget {
                   height: 50,
                   decoration: const BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    color: Colors.amber,
+                    color: Colors.purple,
                   ),
                   child: const Center(
                       child: Text(
-                    'Connexion',
+                    'CONNEXION',
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w400),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   )),
                 ),
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:    const OnboardingPage()
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const OnboardingPage());
   }
 }
-
-
- 
